2ac49a8a9fe6e1129bc08046648a684cd7e8e219
RiceDictator <kevinfritsch7@yahoo.de>
2024-04-23 22:18:50 +0200

    reduced huge performance issues.

--- KleinanzeigenTracker.py
+++ KleinanzeigenTracker.py
@@ -5,16 +5,17 @@ from searchingForString import main
 
 
 chrome_driver_path = "A:\\Desktop\\Scripts\\chromedriver.exe"
-url = "https://www.amazon.de/dp/B0BW9DFPWN?psc=1&ref=ppx_yo2ov_dt_b_product_details"
+url = "https://www.amazon.de/Apple-AirPods-Generation-MagSafe-USB-C/dp/B0CHWZ9TZS/ref=sr_1_3?crid=2SGQMUPKBF890&dib=eyJ2IjoiMSJ9.U2XBkYib7efL-j4R9asbCoryWfD3kbXf7LHWAyyeqqNtTQPTXOHH2NmFD-kTB7MNbBAWDyxGgfmF9RicICZD7XQfZ7NJj2VXV9QIJa8Y5GB92DWbhQ5crQwzOW_0PzzPQhY5fgUUE75FmaZ0PlywWhiEJNV3uaDXyT3BDveWaR18meTGdPRqj86EhHOcUD0Zkco-0P_b6PA33mlPvx-gWBLEFMtbxMDBvkumUE3MTNQ.Lsw2aZF-J3rDFqkgICvrAL3xs54dx9uAyUeHF6kr-ZU&dib_tag=se&keywords=airpods+pro+2&qid=1713902984&sprefix=airpods%2Caps%2C95&sr=8-3&ufe=app_do%3Aamzn1.fos.335e368b-29e8-4542-bb58-939a88195e78"
 
 selector = "#corePriceDisplay_desktop_feature_div > div.a-section.a-spacing-none.aok-align-center.aok-relative > span.a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay > span:nth-child(2) > span.a-price-whole"
 selector2 = "#corePriceDisplay_desktop_feature_div > div.a-section.a-spacing-none.aok-align-center.aok-relative > span.a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay > span:nth-child(2) > span.a-price-fraction"
 
 
-
-
-firstValue = main(url,"text", selector,  chrome_driver_path) #url, selector, chrome_driver_path
-secondValue = main(url,"text", selector2,  chrome_driver_path) #url, selector, chrome_driver_path
-
-firstValue = firstValue + "," + secondValue + "€"
-print(firstValue)
+price = 300
+while price > 244:
+    firstValue = main(url,"text", selector,  chrome_driver_path) #url, selector, chrome_driver_path
+    secondValue = main(url,"text", selector2,  chrome_driver_path) #url, selector, chrome_driver_path
+    price = int(firstValue)
+    firstValue = firstValue + "," + secondValue + "€"
+    print(firstValue)
+    
--- searchingForString.py
+++ searchingForString.py
@@ -1,10 +1,12 @@
 import undetected_chromedriver as uc
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import logging
+
+# Konfigurieren des Loggings
+logging.basicConfig(level=logging.ERROR)
 
 def setup_driver(chrome_driver_path):
     s = Service(executable_path=chrome_driver_path, log_path='NUL')
@@ -12,41 +14,43 @@ def setup_driver(chrome_driver_path):
     chrome_options.add_argument('--log-level=3')
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--disk-cache-dir=/path/to/cache')  # Enable caching
-    driver = uc.Chrome(service=s, options=chrome_options)
+    try:
+        driver = uc.Chrome(service=s, options=chrome_options)
+    except Exception as e:
+        logging.error(f"Driver konnte nicht initialisiert werden: {e}")
+        return None
     return driver
 
 def suche(element_css_selector, driver, typeElement):
+    if driver is None:
+        return None
     try:
         wait = WebDriverWait(driver, 10)  # Adjust wait time as needed
         element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, element_css_selector)))
         href = element.get_attribute('href')
 
         if typeElement == "text":
-            if element.text:
-                return element.text
-            else:
-                print("Element found, but it contains no data.")
+            return element.text if element.text else "Element found, but it contains no data."
         elif typeElement == "href":
-            if href:
-                return href
-            else:
-                print("Element gefunden, aber es enthält keine href-Adresse.")
+            return href if href else "Element gefunden, aber es enthält keine href-Adresse."
         else:
-            print("wrong type " + typeElement)
+            return "wrong type " + typeElement
     except Exception as e:
-        print(f"Fehler: Das Element konnte nicht gefunden werden. {e}")
+        logging.error(f"Fehler: Das Element konnte nicht gefunden werden. {e}")
         return None
 
 def main(url, type, element_css_selector, chrome_driver_path):
     driver = setup_driver(chrome_driver_path)
+    if driver is None:
+        return "Driver konnte nicht initialisiert werden."
     try:
         driver.get(url)
-        result = suche(element_css_selector, driver, type)
-        return result
+        return suche(element_css_selector, driver, type)
     except Exception as e:
-        print(f"Ein unerwarteter Fehler ist aufgetreten: {e}")
+        logging.error(f"Ein unerwarteter Fehler ist aufgetreten: {e}")
     finally:
-        driver.quit()
+        if driver:
+            driver.quit()
 
 if __name__ == "__main__":
     main()  # either href or text
