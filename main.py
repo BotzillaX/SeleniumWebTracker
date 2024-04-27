f1cf669d4a12be818f55247f1c5e494dc2f9a36f
RiceDictator <kevinfritsch7@yahoo.de>
2024-04-27 23:27:02 +0200

    added AI capabillity to the ebay bot for contacting vendor

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from pixelDetection import lookingForPixel
 from time import sleep
 from pyautogui import click
 import json
-from keyboard import press_and_release
+from keyboard import press_and_release, write
+from AIOllama import ollamaGPT
+
 
 with open("C:\\Users\\kevin\\Dropbox\\Kundenwunsch\\Wagner An- und Verkauf\\Kundenwunsch.json", "r") as json_File:
     loadedJSON = json.load(json_File)
@@ -30,7 +32,7 @@ count = 1
 #url, selector, chrome_driver_path
 
 
-currentURLfrommSelector = "#srchrslt-adtable > li:nth-child("+str(count)+") > article > div.aditem-main > div.aditem-main--middle > h2 > a"
+currentSelector = "#srchrslt-adtable > li:nth-child("+str(count)+") > article > div.aditem-main > div.aditem-main--middle > h2 > a"
 currentPrice =  "#srchrslt-adtable > li:nth-child("+str(count)+") > article > div.aditem-main > div.aditem-main--middle > div.aditem-main--middle--price-shipping > p"
 
 
@@ -46,11 +48,17 @@ while price > Wunschpreis:
 
 
 
-urlHref = main(url,"href", currentURLfrommSelector,  chrome_driver_path)
+urlHref = main(url,"href", currentSelector,  chrome_driver_path)
 openBrowser1(urlHref)
 sleep(1)
-clickingOnPixle("message")
+clickingOnPixle("message1")
 sleep(1)
 clickingOnPixle("textField") 
-sleep(1)
-press_and_release("strg+tab")
+
+titel = main(urlHref,"text", "#viewad-title",  chrome_driver_path)
+describ = main(urlHref,"text", "#viewad-description-text",  chrome_driver_path)
+sellerName = main(urlHref,"text", "#viewad-contact > div > ul > li > span > span.text-body-regular-strong.text-force-linebreak.userprofile-vip",  chrome_driver_path)
+
+theMessage = ollamaGPT("act as a buyer who is interested in an item, you only text in german and never in englisch:  TITEL: "+titel+", Beschreibung: "+describ+" und Verkäufer: "+sellerName+", Preis: "+str(price)+".")
+
+write(theMessage)
